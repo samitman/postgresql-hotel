328d345d61cf282b9a09484b65ec1c1ed7856949
Sam Itman <si237@njit.edu>
2023-02-13 10:29:18 -0500

    new user, updated schema

--- schema.sql
+++ schema.sql
@@ -1,13 +1,15 @@
 --- Hotel Reservations DB Schema by Sam Itman
+CREATE USER hotel_admin;
 
 CREATE DATABASE hotel
-    WITH
-    OWNER = postgres;
+GRANT ALL PRIVILEGES ON DATABASE hotel TO hotel_admin;
 
-CREATE SCHEMA hotel;
+\c hotel;
+
+CREATE SCHEMA hotel_schema;
 
 --- Guests Table
-CREATE TABLE hotel.guests(
+CREATE TABLE hotel_schema.guests(
     guest_id SERIAL PRIMARY KEY,
     first_name VARCHAR(45) NOT NULL,
     last_name VARCHAR(45) NOT NULL,
@@ -17,14 +19,14 @@ CREATE TABLE hotel.guests(
 --- Rooms Table
 CREATE TYPE room_category AS ENUM ('single', 'double');
 
-CREATE TABLE hotel.rooms(
+CREATE TABLE hotel_schema.rooms(
     room_id SERIAL PRIMARY KEY,
     category room_category NOT NULL,
     price NUMERIC DEFAULT 100
 );
 
 --- Reservations Table
-CREATE TABLE hotel.reservations(
+CREATE TABLE hotel_schema.reservations(
     reservation_id SERIAL PRIMARY KEY,
     res_guest_id INTEGER references guests(guest_id) NOT NULL,
     res_room_id INTEGER references rooms(room_id) NOT NULL,
